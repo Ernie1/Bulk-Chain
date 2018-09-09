10caf1a4c0cdc1cbef3f093f17beb35fa172c73e
Ernie1 <870949712@qq.com>
2018-09-09 08:46:15 +0800

    firstFormDialog

--- src/views/application/index.vue
+++ src/views/application/index.vue
@@ -1,96 +1,116 @@
 <template>
   <div class="app-container" style="width: 1000px; margin: 0 auto;">
-    <el-steps :active="active" finish-status="success" align-center>
-      <el-step title="填写入库资料" icon="el-icon-edit-outline"></el-step>
-      <el-step title="提交审核" icon="el-icon-upload2"></el-step>
-      <el-step title="货物入库" icon="el-icon-goods"></el-step>
-      <el-step title="生成仓单" icon="el-icon-printer"></el-step>
-    </el-steps>
+    <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
 
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" label-position="left" style="width: 600px; margin: 50px auto">
+    <el-dialog
+      :before-close="handleClose"
+      visible>
       <!--  -->
-      <el-form-item label="申请人信息">
-        <el-form-item prop="name">
-          <md-input v-model="ruleForm.name">姓名</md-input>
+      <el-steps :active="active" finish-status="success" align-center>
+        <el-step title="填写入库资料" icon="el-icon-edit-outline"></el-step>
+        <el-step title="提交审核" icon="el-icon-upload2"></el-step>
+        <el-step title="货物入库" icon="el-icon-goods"></el-step>
+        <el-step title="生成仓单" icon="el-icon-printer"></el-step>
+      </el-steps>
+
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" label-position="left" style="width: 600px; margin: 50px auto">
+        <!--  -->
+        <el-form-item label="申请人信息">
+          <el-form-item prop="name">
+            <md-input v-model="ruleForm.name">姓名</md-input>
+          </el-form-item>
+          <el-form-item prop="phone">
+            <md-input v-model="ruleForm.phone">电话</md-input>
+          </el-form-item>
         </el-form-item>
-        <el-form-item prop="phone">
-          <md-input v-model="ruleForm.phone">电话</md-input>
+        <!--  -->
+        <el-form-item label="客户信息">
+          <el-form-item prop="clientID">
+            <md-input v-model="ruleForm.clientID">客户ID</md-input>
+          </el-form-item>
+          <el-form-item prop="clientName">
+            <md-input v-model="ruleForm.clientName">客户姓名</md-input>
+          </el-form-item>
+          <el-form-item prop="clientPhone">
+            <md-input v-model="ruleForm.clientPhone">客户电话</md-input>
+          </el-form-item>
         </el-form-item>
-      </el-form-item>
+        <!--  -->
+        <el-card>
+          <el-form-item label="货物信息">
+            <el-row>
+              <el-col :span="6">
+                <el-form-item prop="goodsVariety">
+                  <md-input v-model="ruleForm.goodsVariety">品种代号</md-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="3">&nbsp;</el-col>
+              <el-col :span="6">
+                <el-form-item prop="goodsQuantity">
+                  <md-input v-model="ruleForm.goodsQuantity">数量</md-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="3">&nbsp;</el-col>
+              <el-col :span="6">
+                <el-form-item prop="goodsLevel">
+                  <md-input v-model="ruleForm.goodsLevel">品级</md-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <!--  -->
+            <el-row>
+              <el-col :span="11">
+                <el-form-item prop="goodsRegion">
+                  <md-input v-model="ruleForm.goodsRegion">产地</md-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="3">&nbsp;</el-col>
+              <el-col :span="10">
+                <el-form-item prop="goodsTransport">
+                  <md-input v-model="ruleForm.goodsTransport">运输方式</md-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <!--  -->
+            <el-form-item prop="goodsProduceDate">
+              <md-input v-model="ruleForm.goodsProduceDate">生产日期</md-input>
+            </el-form-item>
+            <!--  -->
+            <el-form-item prop="goodsValidityPeriod">
+              <md-input v-model="ruleForm.goodsValidityPeriod">有效期</md-input>
+            </el-form-item>
+            <!--  -->
+            <el-form-item prop="goodsBand">
+              <md-input v-model="ruleForm.goodsBand">品牌</md-input>
+            </el-form-item>
+            <!--  -->
+            <el-form-item prop="goodsPack">
+              <md-input v-model="ruleForm.goodsPack">货物包装</md-input>
+            </el-form-item>
+            <!--  -->
+            <el-form-item prop="goodsRank">
+              <md-input v-model="ruleForm.goodsRank">货物规格</md-input>
+            </el-form-item>
+          </el-form-item>
+        </el-card>
+        <!--  -->
+        <el-form-item label="仓库信息">
+          <el-form-item prop="warehouseID">
+            <md-input v-model="ruleForm.warehouseID">仓库ID</md-input>
+          </el-form-item>
+          <el-form-item prop="inboundPlanTime">
+            <md-input v-model="ruleForm.inboundPlanTime">拟入库时间</md-input>
+          </el-form-item>
+        </el-form-item>
+      </el-form>
       <!--  -->
-      <el-form-item label="客户信息">
-        <el-form-item prop="clientID">
-          <md-input v-model="ruleForm.clientID">客户ID</md-input>
-        </el-form-item>
-        <el-form-item prop="clientName">
-          <md-input v-model="ruleForm.clientName">客户姓名</md-input>
-        </el-form-item>
-        <el-form-item prop="clientPhone">
-          <md-input v-model="ruleForm.clientPhone">客户电话</md-input>
-        </el-form-item>
-      </el-form-item>
-      <!--  -->
-      <el-form-item label="货物信息">
-        <el-row>
-          <el-col :span="6">
-            <el-form-item prop="goodsVariety">
-              <md-input v-model="ruleForm.goodsVariety">品种代号</md-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="3">&nbsp;</el-col>
-          <el-col :span="6">
-            <el-form-item prop="goodsQuantity">
-              <md-input v-model="ruleForm.goodsQuantity">数量</md-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="3">&nbsp;</el-col>
-          <el-col :span="6">
-            <el-form-item prop="goodsLevel">
-              <md-input v-model="ruleForm.goodsLevel">品级</md-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <!--  -->
-        <el-row>
-          <el-col :span="11">
-            <el-form-item prop="goodsRegion">
-              <md-input v-model="ruleForm.goodsRegion">产地</md-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="3">&nbsp;</el-col>
-          <el-col :span="10">
-            <el-form-item prop="goodsTransport">
-              <md-input v-model="ruleForm.goodsTransport">运输方式</md-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <!--  -->
-        <el-form-item prop="goodsProduceDate">
-          <md-input v-model="ruleForm.goodsProduceDate">生产日期</md-input>
-        </el-form-item>
-        <!--  -->
-        <el-form-item prop="goodsValidityPeriod">
-          <md-input v-model="ruleForm.goodsValidityPeriod">有效期</md-input>
-        </el-form-item>
-        <!--  -->
-        <el-form-item prop="goodsBand">
-          <md-input v-model="ruleForm.goodsBand">品牌</md-input>
-        </el-form-item>
-        <!--  -->
-        <el-form-item prop="goodsPack">
-          <md-input v-model="ruleForm.goodsPack">货物包装</md-input>
-        </el-form-item>
-        <!--  -->
-        <el-form-item prop="goodsRank">
-          <md-input v-model="ruleForm.goodsRank">货物规格</md-input>
-        </el-form-item>
-      </el-form-item>
-    </el-form>
-    <!--  -->
-    <div style="width:100%; display: flex;justify-content: center;">
-        <el-button type="success">保存</el-button>
+      <span slot="footer">
+        <el-button type="success" @click="dialogVisible = false">保存</el-button>
         <el-button type="primary" @click="submitForm('ruleForm')">提交审核</el-button>
-    </div>
+      </span>
+    </el-dialog>
+
+    <!--  -->
   </div>
 </template>
 
@@ -106,6 +126,7 @@ export default {
   filters: {},
   data() {
     return {
+      dialogVisible: false,
       active: 0,
       ruleForm: {
         name: "",
@@ -122,7 +143,9 @@ export default {
         goodsValidityPeriod: "",
         goodsBand: "",
         goodsPack: "",
-        goodsRank: ""
+        goodsRank: "",
+        warehouseID: "",
+        inboundPlanTime: ""
       },
       rules: {
         name: [{ required: true, message: "请输入姓名", trigger: "blur" }],
@@ -136,7 +159,9 @@ export default {
         goodsRegion: [{ required: true, message: "请输入产地", trigger: "blur" }],
         goodsTransport: [{ required: true, message: "请输入运输方式", trigger: "blur" }],
         goodsProduceDate: [{ required: true, message: "请输入生产日期", trigger: "blur" }],
-        goodsValidityPeriod: [{ required: true, message: "请输入有效期", trigger: "blur" }]
+        goodsValidityPeriod: [{ required: true, message: "请输入有效期", trigger: "blur" }],
+        warehouseID: [{ required: true, message: "请输入仓库ID", trigger: "blur" }],
+        inboundPlanTime: [{ required: true, message: "请输入拟入库时间", trigger: "blur" }]
       }
     };
   },
@@ -150,6 +175,13 @@ export default {
           return false;
         }
       });
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
     }
   }
 };
